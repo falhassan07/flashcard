9a8c1cdba2b2651905422fc8cdbd2f132bde8e9a
Farouk Alhassan <omarfarouk@farouks-mbp.home>
2024-12-08 18:01:06 -0500

    flipping card mechanism added

--- flashy.py
+++ flashy.py
@@ -8,11 +8,28 @@ BACKGROUND_COLOR = "#B1DDC6"
 data = pd.read_csv("data/french_words.csv")
 # df_dict = {row.French: row.English for (index, row) in data.iterrows()}
 df_dict = data.to_dict(orient='records')
+current_card ={}
 def next_card():
+    global current_card
     current_card = random.choice(df_dict)
+    canvas.itemconfig(card, image=front_card_image)
+    canvas.itemconfig(card_title, text="French",fill="black")
     french_word = current_card["French"]
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=french_word)
+    canvas.itemconfig(card_word, text=french_word,fill="black")
+    window.after(3000, flip_card)
+
+
+
+
+def flip_card():
+    canvas.itemconfig(card, image=back_card_image)
+    canvas.itemconfig(card_title, text="English",fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"],fill="white")
+
+
+
+
+
 
 
 #-----------------------UI SETUP------------------------------#
@@ -21,10 +38,13 @@ window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 
+window.after(3000, flip_card)
+
 #CANVAS
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
-card_image = PhotoImage(file="images/card_front.png")
-card_front = canvas.create_image(400,263,image=card_image)
+front_card_image = PhotoImage(file="images/card_front.png")
+back_card_image = PhotoImage(file="images/card_back.png")
+card = canvas.create_image(400,263,image=front_card_image)
 card_title = canvas.create_text(400,150,font=("Ariel", 40, "italic"), fill='black')
 card_word = canvas.create_text(400,263,font=("Ariel", 60, "bold"), fill='black')
 canvas.grid(column=0, row=0, columnspan=2)
@@ -41,6 +61,11 @@ right_button = Button(image=right_image,highlightbackground=BACKGROUND_COLOR,com
 
 wrong_button.grid(column=0, row=1)
 right_button.grid(column=1, row=1)
+
+#----------------------------------------------------------------#
+
+
+
 
 
 
